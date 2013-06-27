227b3655c6fb5e293d1bc3a4eec04b6b36ae7d95
Pablo Torres <pablo.torres.t@gmail.com>
2013-06-27 17:32:49 +0100

    /quiltro url prefix added to development environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ SampleApp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Set quiltro prefix for URL
+  config.action_controller.relative_url_root = '/quiltro'
 end
