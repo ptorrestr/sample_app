18a7d1be1a21469dd655fd9787042d4f1d64f7ff
Pablo Torres <pablo.torres.t@gmail.com>
2013-07-17 11:51:24 +0100

    new layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,9 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_back_or user
+      #Solve cookie bug first
+      #redirect_back_or root_url
+      redirect_to root_url
     else
       flash.now[:error] = 'Invalid email/password combination'
       render 'new'
